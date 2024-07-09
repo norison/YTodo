92f750f74080d35bb2d79bc327d1f74604f258fa
Ihor Matiev <ihor.matiev@gmail.com>
2024-07-09 19:48:33 +0300

    Added simple test with users controller

---
+++ src/api/YTodo.Api/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YTodo.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        return Ok("Hello from UsersController");
+    }
+}
---
+++ test/YTodo.Api.Tests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+using Microsoft.AspNetCore.Mvc;
+
+using YTodo.Api.Controllers;
+
+namespace YTodo.Api.Tests.Controllers;
+
+[TestFixture]
+public class UsersControllerTests
+{
+    [Test]
+    public void GetUsers_WhenCalled_ThenShouldReturnOk()
+    {
+        // Arrange
+        var controller = new UsersController();
+
+        // Act
+        var result = controller.GetUsers();
+
+        // Assert
+        result
+            .Should()
+            .BeOfType<OkObjectResult>()
+            .Which.Value.Should().Be("Hello from UsersController");
+    }
+}
